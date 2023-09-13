dd4212a454460ca07f3f39980959946e0c81acc8
Arvind Kharwal <karvind629@gmail.com>
2023-09-13 15:44:51 +0530

    Relational Operators

---
+++ Introduction/Relational.java
@@ -0,0 +1,23 @@
+public class Relational 
+{
+    public static void main(String arg[])
+    {
+        int a,b;
+        a = 10;
+        b = 20;
+        System.out.println(a<b);
+        System.out.println(a<=b);
+        System.out.println(a>b);
+        System.out.println(a>=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        // Object 
+        System.out.println("objects support == and != operators");
+        System.out.println(new Data1()==new Data1());
+        System.out.println(new Data1()!=new Data1());
+    }
+    
+}
+class Data1{
+
+}
