2ce9da7790b99013bb2e52655cda3d18b1b92c52
Arvind Kharwal <karvind629@gmail.com>
2023-09-18 16:03:56 +0530

    18 September 2023

---
+++ Introduction/Input.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Input {
+
+    public static void main(String args[])
+    {
+        Scanner S = new Scanner(System.in);
+        System.out.println("Enter the Number: ");
+        int n = S.nextInt();
+        System.out.print("The Number is: ");
+        System.out.println(n);
+    }
+    
+}
