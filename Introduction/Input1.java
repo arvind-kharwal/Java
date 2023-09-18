2885e9318984e5a85b61cc90f4a4d6b9138426dd
Arvind Kharwal <karvind629@gmail.com>
2023-09-18 16:06:24 +0530

    18 September 2023

---
+++ Introduction/Input1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Input1 {
+
+    public static void main(String args[])
+    {
+        Scanner S = new Scanner(System.in);
+        System.out.println("Enter the Name: ");
+        String n = S.nextLine();
+        System.out.print("The Name is: ");
+        System.out.println(n);
+    }
+    
+}
