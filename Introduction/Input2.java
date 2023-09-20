c59eb03cf16c3ae44a0b45d9fc045e12c84e14d3
Arvind Kharwal <karvind629@gmail.com>
2023-09-20 14:22:26 +0530

    20 September 2023

---
+++ Introduction/Input2.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.io.BufferedReader;
+public class Input2 {
+    public static void main(String args[]) throws Exception
+    {
+        System.out.println("Enter the number: ");
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferReader bf = new BufferReader(in);
+        int n = Integer.parseInt(bf.readLine());
+        System.out.println(n);
+    }
+    
+}
