419dd3a1f8102cce1d38c9fb7d47269d61a6afd6
Arvind Kharwal <karvind629@gmail.com>
2023-09-26 11:09:02 +0530

    26 September 2023

---
+++ Introduction/Relational2.java
@@ -0,0 +1,10 @@
+public class Relational2 {
+    public static void main(String args[]) {
+        int a = 100;
+        int b = 200;
+        int c = 0;
+        if (a < 50)
+            c = 300;
+        System.out.println("The values are: " + a + " " + b + " " + c);
+    }
+}
