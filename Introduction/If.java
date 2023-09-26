35cc93aacb60f2de8139fdd7cd54cbb19dfa908e
Arvind Kharwal <karvind629@gmail.com>
2023-09-26 11:06:59 +0530

    26 September 2023

---
+++ Introduction/If.java
@@ -0,0 +1,9 @@
+class If {
+    public static void main(String args[]) {
+        int n = 18;
+        if (n < 10)
+            System.out.println("Hello");
+        else
+            System.out.println("World");
+    }
+}
